5ab4641c06dc9c1c761a4be1b9c0d2606cd45b21
cerka@alex <yurasmirnovlynxtram@gmail.com>
2025-11-26 21:38:43 +0900

    add students bar in hero section

--- src/pages/homePage/sections/HeroSection/ui/HeroSection.tsx
+++ src/pages/homePage/sections/HeroSection/ui/HeroSection.tsx
@@ -8,9 +8,39 @@ export const HeroSection = () => {
         <h1>Первый курс по компьютерной сборке</h1>
         <Timer targetDate={new Date('2030-12-31T23:59:59')}/>
       </div>
-        <div className={css.students_progress}>
-          добавить прогрес бар
-        </div>
+      <StudentsProgress/>
     </section>
+  )
+}
+
+const StudentsProgress = () => {
+  return (
+    <div className={css.students_progress}>
+      <div className={css.students_progress_content}>
+        <button className={css.request_cours}>
+          Заказать курс
+        </button>
+        <div className={css.students_count}>
+          <span >
+            Учеников всего: <span className={css.total_students}> 200</span>
+          </span>
+          <span >
+            Успешно закончили курс: <span className={css.successfully_completed}>190</span>
+          </span>
+        </div>
+        <div className={css.progress_bar_wrapper}>
+          <span className={css.earned_by_students}>
+            Заработано учениками: <span>400 000₽</span>
+          </span>
+          <div className={css.progress_bar}>
+            <div className={css.progress_bar_fill}></div>
+          </div>
+          <div className={css.progress_bar_values}>
+            <span>0₽</span> 
+            <span>1 000 000₽</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
