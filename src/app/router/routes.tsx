6aab405a62de764edc3e79578a9c581ea01f7065
unknown <yurasmirnovlynxtram@gmail.com>
2025-11-26 15:47:40 +0900

    initial commit

---
+++ src/app/router/routes.tsx
@@ -0,0 +1,22 @@
+import type { RouteObject } from 'react-router-dom'; // Импорт типа из react-router-dom
+import { ROUTE_PATHS } from './routesPath';
+
+// Определяем тип для маршрута с мета-информацией
+interface RouteMeta {
+    title?: string;
+    description?: string;
+}
+
+interface AppRoute extends Omit<RouteObject, 'children'> {
+    meta?: RouteMeta;
+}
+
+// Массив маршрутов
+export const routes: AppRoute[] = [
+    // Добавь новые маршруты сюда, например:
+    // {
+    //   path: ROUTE_PATHS.CONTACT,
+    //   element: <Contact />,
+    //   meta: { title: 'Контакты' },
+    // },
+];
